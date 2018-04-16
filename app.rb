135e0f82bbf3849ed1f79bbea32a2fd279da5503
Frank McCallion <fkmccallion@gmail.com>
2018-04-16 13:36:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,5 +5,19 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:name], params[:weight], params[:height])
+
+      erb :"pirates/show"
+    end
+
   end
 end
