9bdabf12a316672b9612b55aac3c6ccfb8005c36
Frank McCallion <fkmccallion@gmail.com>
2018-04-16 15:05:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,19 +10,19 @@ module FormsLab
     end
 
     get '/new' do
-      erb :"pirates/new"
+      erb :"pirates/new" # Directs to the new view form
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
+      @pirate = Pirate.new(params[:pirate]) # Received data from new view form. Send pirate info including nested ship info that will not be used.
 
-      params[:pirate][:ships].each do |ship|
+      params[:pirate][:ships].each do |ship| # Iterate through nested ship info and create new ship objects for each in the array.
         Ship.new(ship)
       end
-      @all_ships = Ship.all
+      @all_ships = Ship.all # Create instance variable type array of all ships.
 
 
-      erb :"pirates/show"
+      erb :"pirates/show" # Redirect to show data using created objects.
     end
 
   end
