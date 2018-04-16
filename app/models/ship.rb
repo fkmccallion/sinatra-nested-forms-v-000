fc309afd879ea493b93a572fc34851d7efaaefe9
Frank McCallion <fkmccallion@gmail.com>
2018-04-16 14:06:57 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,11 +1,13 @@
 class Ship
 
-  attr_accessor :name
+  attr_accessor :name, :type, :booty
 
   @@all = []
 
-  def initialize(name)
+  def initialize(name, type, booty)
     @name = name
+    @type = type
+    @booty = booty
     @@all << self
   end
 
