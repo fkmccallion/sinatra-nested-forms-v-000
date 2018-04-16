cd148d135f4bf7c033bafc83020e703c0ddca58f
Frank McCallion <fkmccallion@gmail.com>
2018-04-16 13:54:38 +0000

    automatically backed up by learn

---
+++ app/models/ship.rb
@@ -0,0 +1,23 @@
+class Ship
+
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(name, weight, height)
+    #binding.pry
+    @name = name
+    @weight = weight
+    @height = height
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
+end
